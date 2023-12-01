80c1db31827b78f0144d98a2f43d17dd30fe0a52
Arpit Pandey <arpitpandey.2001@gmail.com>
2023-12-01 10:37:07 +0530

    Completed Rock, Paper, Scissors project

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,18 +1,73 @@
-function choice(name) {
+let player = "";
+let computer = "";
+let section1 = document.getElementById("section1");
+let section2 = document.getElementById("section2");
+let section3 = document.getElementById("section3");
 
-  console.log(name);
-  let choice = document.getElementById("choice");
+function playerChoice(pchoice) {
+  section1.style.display = "none";
+  imageSelector(pchoice, "pchoice");
+  player = pchoice;
+  computerChoice();
+}
 
-  switch (name) {
-    case "Rock":
-      choice.src = "./images/rock.png";
+function computerChoice() {
+  section2.style.display = "flex";
+  let n = Math.floor(Math.random() * 3 + 1);
+  let cchoice = "";
+  switch (n) {
+    case 1:
+      cchoice = "Rock";
       break;
-    case "Paper":
-      choice.src = "./images/paper.png";
+    case 2:
+      cchoice = "Paper";
       break;
-    case "Scissors":
-      choice.src = "./images/scissors.png";
+    case 3:
+      cchoice = "Scissors";
       break;
   }
 
+  imageSelector(cchoice, "cchoice");
+  computer = cchoice;
+  finalResult();
+}
+
+function imageSelector(choice, element) {
+  let result = document.querySelector(`#${element}`);
+
+  switch (choice) {
+    case "Rock":
+      result.src = "./images/rock.png";
+      break;
+    case "Paper":
+      result.src = "./images/paper.png";
+      break;
+    case "Scissors":
+      result.src = "./images/scissors.png";
+      break;
+  }
+}
+
+function finalResult() {
+  section3.style.display = "block";
+
+  let decision = document.querySelector("#result");
+
+  if (player === computer) {
+    decision.innerHTML = "You Almost Had It! It's a draw!";
+  } else if (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Paper" && computer === "Rock") ||
+    (player === "Scissors" && computer === "Paper")
+  ) {
+    decision.innerHTML = "Congratulations! You Won!";
+  } else {
+    decision.innerHTML = "Better Luck Next Time! You lose!";
+  }
+}
+
+function retry() {
+  section1.style.display = "block";
+  section2.style.display = "none";
+  section3.style.display = "none";
 }
