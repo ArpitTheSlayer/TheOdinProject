3329f9d7f341efb742308ecc8352fad703b3e9ba
Arpit Pandey <arpitpandey.2001@gmail.com>
2024-02-03 16:22:34 +0530

    Random Color Button Functional

--- EtchASketch/script.js
+++ EtchASketch/script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector("#container");
+const gridSize = document.querySelector("#grid-size");
+const randomColors = document.querySelector("#random-colors");
+let isRandomColor = false;
 
 for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
@@ -11,21 +14,37 @@ for (let i = 0; i < 16; i++) {
 }
 
 let pixels = document.querySelectorAll("#container>div>div");
+pixels.forEach((pixel) => displayPixel(pixel));
 
-pixels.forEach((pixel) => {
-  pixel.addEventListener(
-    "mouseover",
-    (e) => (e.target.style.backgroundColor = "red")
-  );
+gridSize.addEventListener("click", makeGrid);
+
+randomColors.addEventListener("click", () => {
+  if (isRandomColor) {
+    isRandomColor = false;
+  } else {
+    isRandomColor = true;
+  }
 });
 
-const gridSize = document.querySelector("#grid-size");
-gridSize.addEventListener("click", makeGrid);
+function displayPixel(pixel) {
+  pixel.addEventListener("mouseover", (e) => {
+    if (isRandomColor) {
+      e.target.style.backgroundColor = generateRandomColor();
+    } else {
+      e.target.style.backgroundColor = "red";
+    }
+  });
+}
 
 function makeGrid() {
   container.innerHTML = "";
-
-  let number = +prompt("Enter grid size:");
+  let number = 101;
+  while (number > 100) {
+    number = +prompt("Enter grid size:");
+    if (number > 100) {
+      alert("Grid size is too big! Grid size should be less than 100.");
+    }
+  }
   for (let i = 0; i < number; i++) {
     const div = document.createElement("div");
     const subDiv = [];
@@ -34,14 +53,14 @@ function makeGrid() {
       div.appendChild(subDiv[j]);
     }
     container.appendChild(div);
-
     pixels = document.querySelectorAll("#container>div>div");
-
-    pixels.forEach((pixel) => {
-      pixel.addEventListener(
-        "mouseover",
-        (e) => (e.target.style.backgroundColor = "red")
-      );
-    });
+    pixels.forEach((pixel) => displayPixel(pixel));
   }
+}
+
+function generateRandomColor() {
+  let r = Math.floor(Math.random() * 255) + 1;
+  let g = Math.floor(Math.random() * 255) + 1;
+  let b = Math.floor(Math.random() * 255) + 1;
+  return `rgb(${r},${g},${b})`;
 }
