4505c5fcb74c9828503053405fb95e9d0b6572ee
Arpit Pandey <arpitpandey.2001@gmail.com>
2024-02-03 13:30:27 +0530

    Made Basic UI

--- NewRockPaperScissors/script.js
+++ NewRockPaperScissors/script.js
@@ -3,74 +3,70 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   switch (randomNumber) {
     case 1:
-      computerSelection = "rock";
+      computerSelection = "Rock";
       break;
 
     case 2:
-      computerSelection = "paper";
+      computerSelection = "Paper";
       break;
 
     case 3:
-      computerSelection = "scissors";
+      computerSelection = "Scissors";
       break;
   }
   return computerSelection;
 }
 
-function getPlayerChoice() {
-  let playerSelection = "";
-  while (true) {
-    playerSelection = prompt("Enter your choice:").toLowerCase();
-    if (
-      !(
-        playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors"
-      )
-    ) {
-      console.log("Invalid choice. Choose again.");
-    } else {
-      break;
-    }
-  }
-  return playerSelection;
-}
-
 function playRound(playerSelection, computerSelection) {
-  console.log(
-    `You chose ${playerSelection} while Computer chose ${computerSelection}.`
-  );
+  div.innerHTML += `<br>You chose ${playerSelection} while Computer chose ${computerSelection}.<br>`;
   if (playerSelection === computerSelection) {
-    console.log("It's a draw. So let's have a rematch.");
-    playRound(getPlayerChoice(), getComputerChoice());
+    div.innerHTML += "It's a draw. Points to none.<br>";
   } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
-    console.log("You Win!");
+    div.innerHTML += "You Win!<br>";
     playerScore++;
   } else {
-    console.log("You Lose!");
+    div.innerHTML += "You Lose!<br>";
     computerScore++;
   }
+  para.textContent = `Player: ${playerScore} vs Computer: ${computerScore}`;
 }
 
-function game() {
-  while (playerScore < 5 && computerScore < 5) {
-    playRound(getPlayerChoice(), getComputerChoice());
-  }
-  console.log(
-    `You scored ${playerScore} points while Computer scored ${computerScore} points.`
-  );
+function gameOver() {
+  buttons.forEach((button) => (button.disabled = true));
+  div.innerHTML = `<br>You scored ${playerScore} points while Computer scored ${computerScore} points.<br>`;
   if (playerScore === 5) {
-    console.log("You won 5 times. So you won the game.");
+    div.innerHTML += "You won 5 times. So you won the game.<br><br>";
   } else {
-    console.log("Computer won 5 times. So you lost the game.");
+    div.innerHTML += "Computer won 5 times. So you lost the game.<br><br>";
   }
+
+  const playAgain = document.createElement("button");
+  playAgain.textContent = "Play Again";
+  div.appendChild(playAgain);
+  playAgain.addEventListener("click", () => {
+    div.innerHTML = "";
+    computerScore = 0;
+    playerScore = 0;
+    buttons.forEach((button) => (button.disabled = false));
+  });
 }
 
 let computerScore = 0;
 let playerScore = 0;
 
-game();
+const buttons = document.querySelectorAll("button");
+const div = document.querySelector("div");
+const para = document.querySelector("p");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.textContent, getComputerChoice());
+    if (!(playerScore < 5 && computerScore < 5)) {
+      gameOver();
+    }
+  });
+});
